18d1cae53333459acac8a808e0604a847357fa73
sindhupallavi14 <sindhuramanadula14@gmail.com>
2024-11-28 13:51:22 +0530

    Navbar updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,19 @@
+import logo from "../assets/logo.png";
+import order from "../assets/order.png";
+import home from "../assets/home.png";
+import cart from "../assets/cart.png";
+
 
 export default function Navbar()
 {
     return(
         <div className="nav">
-
+           <img className="logo" src={logo} alt="Busy buy"/>
+           <div className="navitems">
+            <img className="home" src={home} alt="Busy buy"/>
+            <img className="order" src={order} alt="Busy buy"/>
+            <img className="cart" src={cart} alt="Busy buy"/>
+           </div>
         </div>
     )
 }
