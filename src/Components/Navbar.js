4314ff08b26ee1c9f13fbcbf40334f03e1829c42
sindhupallavi14 <sindhuramanadula14@gmail.com>
2024-11-28 14:24:42 +0530

    Navbar updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,20 +2,47 @@ import logo from "../assets/logo.png";
 import order from "../assets/order.png";
 import home from "../assets/home.png";
 import cart from "../assets/cart.png";
+import logout from "../assets/log-out.png";
+import signin from "../assets/Signin.png";
+import { useState } from "react";
 
-
-export default function Navbar()
-{
-    return(
+export default function Navbar() {
+    const [isloggedin,setIsloggedin]=useState(false);
+    return (
         <div className="nav">
-           <img className="logo" src={logo} alt="Busy buy"/>
-           <div className="navitems">
-            <img className="home" src={home} alt="Busy buy"/>
-            <img className="order" src={order} alt="Busy buy"/>
-            <img className="cart" src={cart} alt="Busy buy"/>
-           </div>
+            <img className="logo" src={logo} alt="Busy buy" />
+ 
+
+           {!isloggedin ? (  <div className="navitems1">
+                <div className="navitem">
+                    <img className="icon" src={home} alt="home" />
+                    <p>Home</p>
+                </div>
+                <div className="navitem">
+                    <img className="icon" src={signin} alt="logout" />
+                    <p>Sign-in</p>
+                </div>
+            </div>
+            ):( <div className="navitems2">
+                <div className="navitem">
+                    <img className="icon" src={home} alt="home" />
+                    <p>Home</p>
+                </div>
+                <div className="navitem">
+                    <img className="icon" src={order} alt="order" />
+                    <p>Order</p>
+                </div>
+                <div className="navitem">
+                    <img className="icon" src={cart} alt="cart" />
+                    <p>Cart</p>
+                </div>
+                <div className="navitem">
+                    <img className="icon" src={logout} alt="logout" />
+                    <p>Logout</p>
+                </div>
+            </div>)}
+          
+           
         </div>
-    )
+    );
 }
-
-
